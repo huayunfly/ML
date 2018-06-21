a8f9f4693692eaac32391ce75ca65a4c0dab6e0b
huayun <huayunfly@126.com>
2018-06-21 16:37:48 +0800

    Add a linear regression test case.

--- py/tensorflowtest.py
+++ py/tensorflowtest.py
@@ -8,6 +8,7 @@ Tensorflow test.
 
 import unittest
 import datetime
+import numpy as np
 import tensorflow as tf
 
 class TensorflowTest(unittest.TestCase):
@@ -64,9 +65,24 @@ class TensorflowTest(unittest.TestCase):
         self.assertEqual(z_val, 13)
 
     def test_linear_regression(self):
-        pass
-        
+        """
+        Linear regression, using nomal matrix.
+        (theta = XT*X)-1*XT*y
+        """
+        from sklearn.datasets import fetch_california_housing
+        housing = fetch_california_housing()
+        m, n = housing.data.shape
+        housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
 
+        X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+        y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+        XT = tf.transpose(X)
+        theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+        with tf.Session() as sess:
+            theta_value = theta.eval()
+            print(theta_value)
+
+        
 if __name__ == '__main__':
     unittest.main()
 
